848f1157736f5c76aa87ed8a236190807623f952
AnbuSelvanAk <166841969+AnbuSelvanAk@users.noreply.github.com>
2024-10-01 16:24:54 +0530

    Add files via upload

---
+++ driver.java
@@ -0,0 +1,30 @@
+class sbi  
+  {
+    double r,amt,Interestst;
+    void calculate interest()
+    {
+        r=0.7;
+        interest=amt*r;
+        (system.out.System.out.println);("interest="+interest);
+    }
+    
+    }
+class axis{
+    doubler,amt,interest;
+    void calculate interest(){
+        r=0.12,amt=10000;
+        interest=amt*r;
+        System.out.println("Interest="+interest);
+    }
+    
+    }
+
+class helloworld{
+    {
+        SBI sbi=new SBI();
+        sbi.calculate Interest();
+        Axis a= new Axis();
+        a.calculate Interest();
+    }
+    
+    }
